626676c981f89ed0bbc350145e390a4d1317be8f
Matias <matalves@gmail.com>
2024-08-20 10:22:32 -0300

    Se crea estructura de proyecto Flight-App

---
+++ src/main/java/org/tokioschool/flightapp/FlightAppApplication.java
@@ -0,0 +1,13 @@
+package org.tokioschool.flightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tokioschool/flightapp/FlightAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tokioschool.flightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
